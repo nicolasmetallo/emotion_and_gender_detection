602fce10b76e2c6e2ed703a8e9d749ef268f0ab3
Nicolas Metallo <nicolas.metallo@nyu.edu>
2018-09-18 03:28:17 -0300

    fixed

--- src/run_demo_on_video.py
+++ src/run_demo_on_video.py
@@ -29,9 +29,10 @@ def create_arg_parser():
 
 def run_emotion_gender_detector(input_video, path_to_extract, path_to_predict, output_video):
     try:
-      os.mkdir(path_to_extract)
-    except:
-      pass
+        os.mkdir(path_to_extract)
+    except Exception as e:
+        print(e)
+        pass
 
     # parameters for loading data and images
     image_path = path_to_extract
@@ -58,79 +59,79 @@ def run_emotion_gender_detector(input_video, path_to_extract, path_to_predict, o
     gender_target_size = gender_classifier.input_shape[1:3]
 
     print("Extracting frames from video...")
-    os.system('ffmpeg -i {} -vf fps=30 {}/thumb%04d.png'.format(input_video, image_path)
+    os.system('ffmpeg -i {} -vf fps=30 {}/thumb%04d.png'.format(input_video, image_path))
 
-    images = glob.glob(os.path.join(image_path,"*.png"))
+    images = glob.glob(os.path.join(image_path, "*.png"))
     images.sort()
 
     # Check if dir exists then remove, if not, then make dir
     if os.path.exists(path_to_predict):
-      shutil.rmtree(path_to_predict)
+        shutil.rmtree(path_to_predict)
 
     try:
-      os.mkdir(path_to_predict)
-    except:
-      pass
+        os.mkdir(path_to_predict)
+    except Exception as e:
+        print(e)
+        pass
 
     for i in trange(len(images)):
-      # loading images
-      rgb_image = load_image(images[i], grayscale=False)
-      gray_image = load_image(images[i], grayscale=True)
-      gray_image = np.squeeze(gray_image)
-      gray_image = gray_image.astype('uint8')
-      faces = detect_faces(face_detection, gray_image)
-      for face_coordinates in faces:
-          x1, x2, y1, y2 = apply_offsets(face_coordinates, gender_offsets)
-          rgb_face = rgb_image[y1:y2, x1:x2]
+        # loading images
+        rgb_image = load_image(images[i], grayscale=False)
+        gray_image = load_image(images[i], grayscale=True)
+        gray_image = np.squeeze(gray_image)
+        gray_image = gray_image.astype('uint8')
+        faces = detect_faces(face_detection, gray_image)
+        for face_coordinates in faces:
+            x1, x2, y1, y2 = apply_offsets(face_coordinates, gender_offsets)
+            rgb_face = rgb_image[y1:y2, x1:x2]
+            x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
+            gray_face = gray_image[y1:y2, x1:x2]
 
-          x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
-          gray_face = gray_image[y1:y2, x1:x2]
+            try:
+                rgb_face = cv2.resize(rgb_face, (gender_target_size))
+                gray_face = cv2.resize(gray_face, (emotion_target_size))
+            except Exception as e:
+                print(e)
+                continue
 
-          try:
-              rgb_face = cv2.resize(rgb_face, (gender_target_size))
-              gray_face = cv2.resize(gray_face, (emotion_target_size))
-          except:
-              continue
+            rgb_face = preprocess_input(rgb_face, False)
+            rgb_face = np.expand_dims(rgb_face, 0)
+            gender_prediction = gender_classifier.predict(rgb_face)
+            gender_label_arg = np.argmax(gender_prediction)
+            gender_text = gender_labels[gender_label_arg]
 
-          rgb_face = preprocess_input(rgb_face, False)
-          rgb_face = np.expand_dims(rgb_face, 0)
-          gender_prediction = gender_classifier.predict(rgb_face)
-          gender_label_arg = np.argmax(gender_prediction)
-          gender_text = gender_labels[gender_label_arg]
+            gray_face = preprocess_input(gray_face, True)
+            gray_face = np.expand_dims(gray_face, 0)
+            gray_face = np.expand_dims(gray_face, -1)
+            emotion_prediction = emotion_classifier.predict(gray_face)
+            emotion_probability = np.max(emotion_prediction)
+            emotion_label_arg = np.argmax(emotion_prediction)
+            emotion_text = emotion_labels[emotion_label_arg]
 
-          gray_face = preprocess_input(gray_face, True)
-          gray_face = np.expand_dims(gray_face, 0)
-          gray_face = np.expand_dims(gray_face, -1)
-          emotion_prediction = emotion_classifier.predict(gray_face)
-          emotion_probability = np.max(emotion_prediction)
-          emotion_label_arg = np.argmax(emotion_prediction)
-          emotion_text = emotion_labels[emotion_label_arg]
+            if emotion_text == 'angry':
+                color = emotion_probability * np.asarray((255, 0, 0))
+            elif emotion_text == 'sad':
+                color = emotion_probability * np.asarray((0, 0, 255))
+            elif emotion_text == 'happy':
+                color = emotion_probability * np.asarray((255, 255, 0))
+            elif emotion_text == 'surprise':
+                color = emotion_probability * np.asarray((0, 255, 255))
+            else:
+                color = emotion_probability * np.asarray((0, 255, 0))
 
-          if emotion_text == 'angry':
-              color = emotion_probability * np.asarray((255, 0, 0))
-          elif emotion_text == 'sad':
-              color = emotion_probability * np.asarray((0, 0, 255))
-          elif emotion_text == 'happy':
-              color = emotion_probability * np.asarray((255, 255, 0))
-          elif emotion_text == 'surprise':
-              color = emotion_probability * np.asarray((0, 255, 255))
-          else:
-              color = emotion_probability * np.asarray((0, 255, 0))
+            color = color.astype(int)
+            color = color.tolist()
 
-          color = color.astype(int)
-          color = color.tolist()
+            draw_bounding_box(face_coordinates, rgb_image, color)
+            draw_text(face_coordinates, rgb_image, gender_text, color, 0, -20, 1, 2)
+            draw_text(face_coordinates, rgb_image, emotion_text, color, 0, -50, 1, 2)
 
-          draw_bounding_box(face_coordinates, rgb_image, color)
-          draw_text(face_coordinates, rgb_image, gender_text, color, 0, -20, 1, 2)
-          draw_text(face_coordinates, rgb_image, emotion_text, color, 0, -50, 1, 2)
-
-      bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
-      cv2.imwrite(path_to_predict + '/predicted%04d.jpg' % i, bgr_image)
+        bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
+        cv2.imwrite(path_to_predict + '/predicted%04d.jpg' % i, bgr_image)
 
     print("Done! Merging frames into video...\n")
     # convert frames to video using FFMPEG
-    os.system('ffmpeg -r 25 -f image2 -i {}/predicted%04d.jpg \
-      -vcodec libx264 -crf 25 -pix_fmt yuv420p {}/output_video.mp4'.format(path_to_predict, path_to_predict))
+    os.system('ffmpeg -r 25 -f image2 -i {}/predicted%04d.jpg -vcodec libx264 -crf 25 -pix_fmt yuv420p {}/output_video.mp4'.format(path_to_predict, path_to_predict))
 
 if __name__ == "__main__":
     arg_parser = create_arg_parser()
